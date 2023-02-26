43fb403565c5640afe0476288d91a39a4d5120d5
+ <+>
2023-02-26 16:07:14 +0300

    code challenge

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
-class Procuct < ActiveRecord::Base
+class Product < ActiveRecord::Base
   has_many :reviews
   has_many :users, through: :reviews
 
@@ -11,7 +11,7 @@ class Procuct < ActiveRecord::Base
       puts "Review for #{name} by #{review.user.name}: #{review.star_rating}. #{review.comment}"
     end
   end
-  
+
   def average_rating
     reviews.average(:star_rating).to_f
   end
